fbb7856fdc23c34cd871599880bd42ef01234d3e
Le van duy tung <duytunglevan@gmail.com>
2026-08-22 15:00:14 +0700

    lvdtungLesson01

---
+++ letungmvc/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using letungmvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace letungmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ letungmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace letungmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
